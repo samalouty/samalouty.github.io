8224e5a360863e1bbce5dac4d9ec74b2f4b8cc2e
samalouty <128055267+samalouty@users.noreply.github.com>
2023-10-18 00:01:44 +0300

    updated hero section
    actual about me

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -9,8 +9,9 @@ function HeroSection() {
   return (
     <div className='hero-container'>
  <video src='\videos\video-3.mp4' autoPlay loop muted />
-       <h1>Abdelrahman Elsamalouty</h1>
-      <p>Web Developer, average KFC enjoyer , etc.</p>
+       <h1>About Me</h1>
+      <p>I'm Abdelrahman Elsamalouty, a web developer who is devoted to what he does. I'm a student at GUC currently studying Computer Science and Engineering. I design user-friendly websites and applications using my knowledge in React, HTML, CSS, and JavaScript. My proficiency with Git and GitHub guarantees efficient code management. My creativity and inventiveness in web development are fueled by this desire. I enjoy learning about new computer science areas while I'm not developing websites.
+</p>
       <div className='hero-btns'>
         <Button className = 'btns' buttonStyle= 'btn--outline'
         buttonSize= 'btn--large'>HIRE ME</Button>
