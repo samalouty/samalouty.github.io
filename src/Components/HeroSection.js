f4ffa95283a5bd438a15ae1fadeb406fbcbb917f
samalouty <128055267+samalouty@users.noreply.github.com>
2023-10-18 04:40:11 +0300

    removed video background for image

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -8,7 +8,7 @@ import '../App.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
- <video src='\videos\video-3.mp4' autoPlay loop muted />
+ {/* <video src='\videos\video-3.mp4' autoPlay loop muted /> */}
        <h1>About Me</h1>
       <p>I'm Abdelrahman Elsamalouty, a web developer who is devoted to what he does. I'm a student at GUC currently studying Computer Science and Engineering. I design user-friendly websites and applications using my knowledge in React, HTML, CSS, and JavaScript. My proficiency with Git and GitHub guarantees efficient code management. My creativity and inventiveness in web development are fueled by this desire. I enjoy learning about new computer science areas while I'm not developing websites.
 </p>
